8db4f444b86c3357e213b6904291f549839b47e2
Mark Faust <markfaust123@gmail.com>
2023-06-23 11:34:33 -0400

    programmatically changed the UI, made left die turn to 6 and reduced alpha (opacity)

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     
+    // activates when the view on phone loads up
+    // (condition: when app first loads on phone)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // who.what = value
+        // currently just setting the image property of the image view
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        
+        // changing alpha of the imageView1 (opacity)
+        diceImageView1.alpha = 0.5
     }
     
     
